d7794cff9875aa7f6f57ea861dbe10af91a0c09a
Reema <it.reema7@gmail.com>
2019-05-21 21:41:59 +0300

    Add files via upload

---
+++ Countries/Countries/DetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DetailViewController.swift
+//  Countries
+//
+//  Created by mac_os on 16/09/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+            imageView.layer.borderColor = UIColor.lightGray.cgColor
+            imageView.layer.borderWidth = 1
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+}
---
+++ Countries/Countries/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by mac_os on 16/09/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var countries = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Countries"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "country", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row].uppercased()
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = countries[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+
+}
+
